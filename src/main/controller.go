db369f5ca925b00b74a1176bb7b6b4e1b11a7cbc
rscibelli <scibellrob@gmail.com>
2023-01-27 16:21:33 -0500

    change port back

--- src/main/controller.go
+++ src/main/controller.go
@@ -11,8 +11,8 @@ func main() {
 
 	http.HandleFunc("/trump-quote", GetDumbQuote)
 
-	fmt.Println("Starting server at port 8081")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
