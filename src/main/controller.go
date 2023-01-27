196b17da70d24437a0f4d73ad40eb19c7e01d3bd
rscibelli <scibellrob@gmail.com>
2023-01-27 14:47:33 -0500

    add a root path

--- src/main/controller.go
+++ src/main/controller.go
@@ -7,10 +7,17 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", HelloWorld)
+
 	http.HandleFunc("/trump-quote", GetDumbQuote)
 
 	fmt.Println("Starting server at port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	msg := "Hello World!"
+	fmt.Fprintf(w, string(msg))
 }
